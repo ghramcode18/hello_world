ec976e294a2e3b0a94dbc304b2eeeedbfaeb6ac8
ghram <ghramalhajyalhajy@gmail.com>
2023-09-12 20:01:59 +0300

    -Delete unuse line

--- src/car/dto/create-car.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateCarDto {
-  id: number;
-  name: string;
-  type: string;
-  isSale: boolean;
-}
--- src/car/dto/update-car.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCarDto } from './create-car.dto';
-
-export class UpdateCarDto extends PartialType(CreateCarDto) {
-  id: number;
-  name: string;
-  type: string;
-  isSale: boolean;
-}
